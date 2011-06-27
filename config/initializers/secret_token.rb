1d6dd23c11894331025ca6033e8767560f65c80d
SailorMercury <mizunoyukino@hotmail.com>
2011-06-28 01:38:31 +0800

    restarting project, removing all files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Yabdi::Application.config.secret_token = '6201d7d95a91681e6cce37e27a1499204036f95fa846ec12f13a0122c977844b744b74f44074207b6eadb254103887dd021dc4c813e6f790169ae9e60c9d66c0'
--- features/support/factories.rb
+++
@@ -1,4 +0,0 @@
-Factory.define :question do |f|
-  f.sequence(:name) { |n| "Pencil#{n}" }
-  f.description "Pencil that draws nicely"
-end
