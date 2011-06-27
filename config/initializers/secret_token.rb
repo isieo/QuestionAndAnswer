a8bf39e77d164e4ddcbad860833487f1c4c80081
SailorMercury <mizunoyukino@hotmail.com>
2011-06-28 01:40:35 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yabdi::Application.config.secret_token = 'ee67d91370be920d96f3a23c144098297c2e510bcfa7de204681a6452ef0c5cc71c6897954fb942ded08adde581c45e7bd8d2509944261b7c8ec8d2ec8a4fec2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yabdi::Application.config.session_store :cookie_store, :key => '_yabdi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yabdi::Application.config.session_store :active_record_store
