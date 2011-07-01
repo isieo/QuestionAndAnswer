fa0f5c14af331ade5b00c80e0021ba3c5d252999
SailorMercury <mizunoyukino@hotmail.com>
2011-07-01 21:42:36 +0800

    initial commit rails yabdi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yabdi::Application.config.secret_token = '78546e8530ed4411c4322cbf676c448b14ee567ac2b360583ee190ee9f7b0314c94d3f305bfda6a57e7380c3be7abc2241d6c3dd9e91d97d333935beb357ac61'
